31670feac9746d20328d51a8a8091da639b33672
nabilafrhn <nabilafarahana01@gmail.com>
2022-03-28 17:24:13 +0700

    yeay

---
+++ src/inheritance1/Hasil.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package inheritance1;
+
+/**
+ *
+ * @author ASUS VIVOBOOK 14
+ */
+public class Hasil {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+
+        Anak dina=new Anak();
+        dina.setnama("Dinaa");
+        dina.Cetak();
+    }
+
+}
+
